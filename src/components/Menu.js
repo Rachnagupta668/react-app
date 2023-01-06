28a6b57ec9ba5f7e6fe268c2729af22b6f1b393d
Rachna <rachnakum0476@gmail.com>
2023-01-06 18:13:57 +0530

    first commit

---
+++ src/components/Menu.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div className="about">
+    <div className="container">
+    <div className="row">
+    <div className="col-6">
+   <div className="about__img">
+   <img src="/img/about.jpg" alt="about" />
+   </div>
+   </div>
+    <div className="col-6 p-25">
+    <h3>The Pizza Menu</h3>
+    <h1>CHICAGO
+    THIN CRUST</h1>
+   
+    <div className="about__btn">
+    <a href="" className="btn btn-smart">
+    READ MORE
+    </a>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default Menu
---
+++ src/components/Prices.js
@@ -0,0 +1,85 @@
+import React from 'react'
+
+const Prices = () => {
+  return (
+    <div className="prices">
+    <div className="container">
+    <div className="row">
+    <div className="col-4">
+    <div className="Prices">
+    <div className="Prices__img">
+    <img src="/img/pizza1.png" alt="pizza1" />
+    </div>
+   <h1 className ="Prices__hadding">
+   Pizza Margeritta
+   </h1>
+  <p className="price__text">
+  Nullam nibh sem, imperdiet ultrices commodo a, vulputate vel ligula. Duis venenatis at eros sed egestas. Mauris rutrum quam risus, vel hendrerit dui tempor in.
+  </p>
+  <p className="prices_rs">$50.00</p>
+    </div>
+    </div>
+
+
+
+
+
+
+
+    <div className="col-4">
+    <div className="Prices">
+    <div className="Prices__img">
+    <img src="/img/pizza2.png" alt="pizza2" />
+    </div>
+   <h1 className ="Prices__hadding">
+   Beer Pizza Crust
+   </h1>
+  <p className="price__text">
+  Nullam nibh sem, imperdiet ultrices commodo a, vulputate vel ligula. Duis venenatis at eros sed egestas. Mauris rutrum quam risus, vel hendrerit dui tempor in.
+  </p>
+  <p className="prices_rs">$25.00</p>
+    </div>
+    </div>
+
+
+
+
+
+
+    <div className="col-4">
+    <div className="Prices">
+    <div className="Prices__img">
+    <img src="/img/pizza3.png" alt="pizza3" />
+    </div>
+   <h1 className ="Prices__hadding">
+   Crazy Crust Pizza
+   </h1>
+  <p className="price__text">
+  Nullam nibh sem, imperdiet ultrices commodo a, vulputate vel ligula. Duis venenatis at eros sed egestas. Mauris rutrum quam risus, vel hendrerit dui tempor in.
+  </p>
+  <p className="prices_rs">$29.00</p>
+    </div>
+    </div>
+ <div className="col-4">
+    <div className="Prices">
+    <div className="Prices__img">
+    <img src="/img/pizza2.png" alt="pizza2" />
+    </div>
+   <h1 className ="Prices__hadding">
+   Pizza Biscuit Bake
+   </h1>
+  <p className="price__text">
+  Nullam nibh sem, imperdiet ultrices commodo a, vulputate vel ligula. Duis venenatis at eros sed egestas. Mauris rutrum quam risus, vel hendrerit dui tempor in.
+  </p>
+  <p className="prices_rs">$49.00</p>
+    </div>
+    </div>
+
+
+   </div>
+   </div>
+  </div>
+  );
+};
+
+export default Prices
